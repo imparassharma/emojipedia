c58d19c3187947ec3e539e05879e708498eae57d
Paras Sharma <parasangiras121@gmail.com>
2022-06-25 12:01:30 +0530

    functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import Main from './components/main';
-
+import detail from './components/detail';
 const rootElement = document.getElementById("root");
 const Root = createRoot(rootElement);
 
 Root.render(
   <Main/>
 )
+
+const new_detail = detail.map(function(x){
+  return x.text.substring(0,5);
+})
+
+console.log(new_detail)
