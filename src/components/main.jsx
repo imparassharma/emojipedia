cedb4744dbee845020d3a9373603c6253fad831b
Paras Sharma <parasangiras121@gmail.com>
2022-06-25 21:54:20 +0530

    mapping

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,24 +2,19 @@ import React from "react";
 import Emojibox from "./emojibox";
 import detail from "./detail";
 
-
-function create_box(box){
-    return(
-        <Emojibox
-            key={box.id}
-            img={box.emoji}
-            name={box.name}
-            text={box.text}
-        />
-    )
-}
-
 function Main(){
     return(
         <div className="main">
             <h1>Emojipedia</h1>
             <div className="container">
-                {detail.map(create_box)}
+                {detail.map(box=>(
+                <Emojibox
+                    key={box.id}
+                    img={box.emoji}
+                    name={box.name}
+                    text={box.text}
+                />
+                ))}
             </div>
         </div>
     )
